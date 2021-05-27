1cb97a91534e140165f5acc674e30b96698eb926
Reyaj Alam <mdreyajalal0007@gmail.com>
2021-05-27 23:52:06 +0530

    1st

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,45 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import {HomeComponent} from './components/home/home.component';
+import {NavbarComponent} from './components/navbar/navbar.component';
+import { AboutusComponent } from './components/aboutus/aboutus.component';
+import { CoursesComponent } from './components/courses/courses.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { UpcomingComponent } from './components/upcoming/upcoming.component'
+const routes: Routes = [
+
+{
+  path : 'home',
+  component : HomeComponent
+},
+{
+  path : 'navbar',
+  component : NavbarComponent
+}, 
+
+{
+  path: 'aboutus',
+  component : AboutusComponent
+},
+{
+  path : 'courses',
+  component: CoursesComponent
+},
+{
+path : 'contact',
+component : ContactComponent
+},
+{
+path : 'upcoming',
+component : UpcomingComponent
+},
+{
+  path : '**',
+  component : HomeComponent
+}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {ReactiveFormsModule} from '@angular/forms';
+import {FormsModule} from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { CoursesComponent } from './components/courses/courses.component';
+import { AboutusComponent } from './components/aboutus/aboutus.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { UpcomingComponent } from './components/upcoming/upcoming.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    CoursesComponent,
+    AboutusComponent,
+    ContactComponent,
+    UpcomingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
